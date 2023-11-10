322b68f83b1458b7e85499557b1428a2b944f099
Alfreedom <00tango.bromine@icloud.com>
2023-11-10 18:28:01 +0100

    bumped version

--- lib/constants/namespaces.dart
+++ lib/constants/namespaces.dart
@@ -1,37 +1,37 @@
-// import 'package:walletconnect_flutter_v2/walletconnect_flutter_v2.dart';
+import 'package:walletconnect_flutter_v2/walletconnect_flutter_v2.dart';
 
-// class NamespaceConstants {
-//   static const Map<String, RequiredNamespace> ethereum = {
-//     'eip155': RequiredNamespace(
-//       methods: [
-//         'eth_sendTransaction',
-//         // 'eth_signTransaction',
-//         'personal_sign',
-//         'eth_sign',
-//         'eth_signTypedData',
-//       ],
-//       chains: ['eip155:1'],
-//       events: [
-//         'chainChanged',
-//         'accountsChanged',
-//       ],
-//     ),
-//   };
+class NamespaceConstants {
+  static const Map<String, RequiredNamespace> ethereum = {
+    'eip155': RequiredNamespace(
+      methods: [
+        'eth_sendTransaction',
+        // 'eth_signTransaction',
+        'personal_sign',
+        'eth_sign',
+        'eth_signTypedData',
+      ],
+      chains: ['eip155:1'],
+      events: [
+        'chainChanged',
+        'accountsChanged',
+      ],
+    ),
+  };
 
-//   static const Map<String, RequiredNamespace> polygon = {
-//     'eip155': RequiredNamespace(
-//       methods: [
-//         'eth_sendTransaction',
-//         'eth_signTransaction',
-//         'personal_sign',
-//         // 'eth_sign',
-//         'eth_signTypedData',
-//       ],
-//       chains: ['eip155:137'],
-//       events: [
-//         'chainChanged',
-//         'accountsChanged',
-//       ],
-//     ),
-//   };
-// }
+  static const Map<String, RequiredNamespace> polygon = {
+    'eip155': RequiredNamespace(
+      methods: [
+        'eth_sendTransaction',
+        'eth_signTransaction',
+        'personal_sign',
+        // 'eth_sign',
+        'eth_signTypedData',
+      ],
+      chains: ['eip155:137'],
+      events: [
+        'chainChanged',
+        'accountsChanged',
+      ],
+    ),
+  };
+}
