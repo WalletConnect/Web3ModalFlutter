592197dfb6f1e7578ae1e8fa755014c8a1441fbb
Alfreedom <00tango.bromine@icloud.com>
2023-11-17 17:17:21 +0100

    dartdocs changes

--- lib/web3modal_flutter.dart
+++ lib/web3modal_flutter.dart
@@ -6,6 +6,7 @@ export 'package:walletconnect_flutter_v2/walletconnect_flutter_v2.dart';
 
 /// Models
 export 'models/w3m_chain_info.dart';
+export 'models/w3m_wallet_info.dart';
 
 /// Utils
 export 'constants/eth_constants.dart';
