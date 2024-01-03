1ec06bed703bb6c66ed2eece15810e3ba77f2879
Alfreedom <00tango.bromine@icloud.com>
2024-01-03 16:28:35 +0100

    rollback script

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,36 +1,23 @@
 #!/bin/bash
 
-# echo ' 🔄 Updating dependencies...'
-# flutter clean
-# flutter pub get
-# flutter pub run build_runner build --delete-conflicting-outputs
+echo ' 🔄 Updating dependencies...'
+flutter clean
+flutter pub get
+flutter pub run build_runner build --delete-conflicting-outputs
 
-# cd ios
+cd ios
 
-# pod install
+pod install
 
-# cd ..
+cd ..
 
-# cd example
+cd example
 
-# echo ' ⬇️ Getting dependencies...'
-# flutter clean
-# flutter pub get
-# flutter pub run build_runner build --delete-conflicting-outputs
+echo ' ⬇️ Getting dependencies...'
+flutter clean
+flutter pub get
+flutter pub run build_runner build --delete-conflicting-outputs
 
-# cd ios
+cd ios
 
-# pod install
-
-
-# Get app version from file
-# PUBSPEC_FILE=/Users/alfreedom/Development/WalletConnect/Web3ModalFlutter/pubspec.yaml
-EXAMPLE_PUBSPEC_FILE=/Users/alfreedom/Development/WalletConnect/Web3ModalFlutter/example/pubspec.yaml
-
-FILE_VALUE=$(echo | grep "^version: " $EXAMPLE_PUBSPEC_FILE)
-PARTS=(${FILE_VALUE//:/ })
-FULL_VERSION=${PARTS[1]}
-VERSION_NUMBER=(${FULL_VERSION//-/ })
-
-# Build ios app with flutter
-echo "flutter build ios --build-name $VERSION_NUMBER --dart-define=\"PROJECT_ID=$PROJECT_ID\" --config-only --release"
+pod install
